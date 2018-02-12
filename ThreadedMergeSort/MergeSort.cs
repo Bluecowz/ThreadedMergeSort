b671bb8210f3a48a97e76341efbd0f81baba7597
Bluecowz <taurenmike@gmail.com>
2018-02-11 22:33:09 -0600

    Added the merge sort. Added the threading for sorting the array.

---
+++ ThreadedMergeSort/MergeSort.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ThreadedMergeSort
+{
+    class MergeSort
+    {
+        private Random rand;
+
+        public MergeSort()
+        {
+            rand = new Random();
+        }
+
+        public int[] Sort(int[] array)
+        {
+            if (array.Length <= 1)
+                return array;
+
+            int middle = rand.Next(array.Length);
+            int[] derp1 = new int[middle];
+            int[] derp2 = new int[array.Length - middle];
+
+            int j = 0;
+            for (int i = 0; i < middle; i++)
+            {
+                derp1[i] = array[j++];
+            }
+
+            for (int i = 0; j < array.Length; i++)
+            {
+                derp2[i] = array[j++];
+            }
+
+            Task<int[]> future1 = Task.Factory.StartNew<int[]>(() => Sort(derp1));
+            Task<int[]> future2 = Task.Factory.StartNew<int[]>(() => Sort(derp2));
+
+            derp1 = future1.Result;
+            derp2 = future2.Result;
+            return Task.Factory.StartNew<int[]>(() => Merge(derp1, derp2)).Result;
+        }
+        
+
+        private int[] Merge(int[] left, int[] right)
+        {
+            int[] derp = new int[left.Length + right.Length];
+            int i = 0, j = 0, k = 0;
+            while((i < left.Length) && (k < right.Length))
+            {
+                if (left[i] < right[k])
+                {
+
+                    derp[j] = left[i];
+                    i++;
+                }
+                else
+                {
+                    derp[j] = right[k];
+                    k++;
+                }
+                j++;
+            }
+            while(i < left.Length)
+            {
+                derp[j] = left[i];
+                j++; i++;
+            }
+
+            while(k < right.Length)
+            { 
+                derp[j] = right[k];
+                j++; k++;
+            }
+
+            return derp;
+        }
+    }
+}
--- ThreadedMergeSort/Program.cs
+++ ThreadedMergeSort/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Diagnostics;
 
 namespace ThreadedMergeSort
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MergeSort sort = new MergeSort();
+            Random rand = new Random();
+            int[] derp = new int[rand.Next(1000000)];
+            Debug.WriteLine("Size: " + derp.Length.ToString());
+            for (int i = 0; i < derp.Length; i++)
+                derp[i] = rand.Next(1000000);
+            Debug.WriteLine("Done populating");
+            derp = sort.Sort(derp);
+            Test(derp);
+            Debug.WriteLine("Success!");
+        }
+
+        private static void Test(int[] array)
+        {
+            
+            for (int i = 1; i < array.Length; i++)
+            {
+                int one = array[i - 1];
+                int two = array[i];
+                if (one > two)
+                    throw new Exception();
+            }
         }
     }
 }
